f74d16a632f779fa723979facd816efddc5155df
JulietaNatalia <julietanataliacabezas@gmail.com>
2023-08-21 15:43:48 -0300

    Cambios

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 4300;
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, './public')));
 
-
-app.listen(PORT, ()=>{
-    console.log('Servidor funcionando ${PORT}');
+app.get('/', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
